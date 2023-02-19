0b17df676d052f7afe0c35a9eb18d955e1f487ad
Pedro Henrique <91342788+devphenrique@users.noreply.github.com>
2023-02-19 19:07:22 -0300

    Add files via upload

---
+++ Desafios 1/desafio-random.py
@@ -0,0 +1,14 @@
+import random
+
+# Desafio 1
+
+moeda = ['Cara', 'Coroa']
+print(random.choices(moeda))
+
+# Desafio 2
+
+nomes = ['João', 'Pedro', 'Henrique', 'Gabriel', 'Milena']
+print(random.choices(nomes))
+
+# Desafio 3
+print(random.randint(10, 100))
